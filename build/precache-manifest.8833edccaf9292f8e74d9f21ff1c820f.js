273e56b104d42bb315bd0f7270cbf49ac3425846
14WL15 <katherinewjing.li@gmail.com>
2021-01-17 05:41:31 -0500

    Initial commit

---
+++ build/precache-manifest.8833edccaf9292f8e74d9f21ff1c820f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3ae1aa57a535e48ef066b4135054da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4463b108f372e567d5f6",
+    "url": "/static/js/2.4efcaf87.chunk.js"
+  },
+  {
+    "revision": "27cc4f7932c9d767331a",
+    "url": "/static/js/main.dc4cf185.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
